eb4d069b01589aeb22578a969a3015a5e16a80a9
fuchun198435163.com <tank198435@163.com>
2021-01-13 11:07:16 +0800

    [fuchun] #test add test

---
+++ lean_algorithm/src/test/java/com/tank/algorithm/sorting/MergeSortingTest.java
@@ -0,0 +1,31 @@
+package com.tank.algorithm.sorting;
+
+import lombok.val;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import static org.junit.jupiter.api.TestInstance.Lifecycle;
+
+/**
+ * @author tank198435163.com
+ */
+@TestInstance(Lifecycle.PER_CLASS)
+class MergeSortingTest {
+
+  @Test
+  void sort() {
+    Assertions.assertNotNull(this.mergeSorting);
+    val result = this.mergeSorting.sort(new int[]{2, 1, 8});
+    Assertions.assertNotNull(result);
+    Assertions.assertTrue(result.length > 0);
+  }
+
+  @BeforeEach
+  void initialize() {
+    this.mergeSorting = new MergeSorting();
+  }
+
+  private MergeSorting mergeSorting;
+}
