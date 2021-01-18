d5dda276f68bd9cf962e74dd5a176067a62553e5
fuchun198435163.com <tank198435@163.com>
2021-01-18 18:25:39 +0800

    [fuchun] #refactor

---
+++ lean_algorithm/src/main/java/com/tank/sorting/Sorting.java
@@ -0,0 +1,15 @@
+package com.tank.sorting;
+
+/**
+ * @param <T>
+ * @author tank198435163.com
+ */
+public interface Sorting<T extends Comparable<T>> {
+
+  /**
+   * @return
+   */
+  T[] sort();
+
+}
+
