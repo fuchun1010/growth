d91dc77516f30f0b21d06d8a5cfa4310a7e3fd45
fuchun198435163.com <tank198435@163.com>
2021-01-19 18:50:36 +0800

    [fuchun] #tech simple quick sort

--- lean_algorithm/src/test/java/com/tank/sorting/QuickSortingTest.java
+++ lean_algorithm/src/test/java/com/tank/sorting/QuickSortingTest.java
@@ -1,10 +1,8 @@
 package com.tank.sorting;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.*;
 
+@DisplayName("快排")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class QuickSortingTest {
 
