1bd7d492a8f7e5166530caa0615ee251ad8a8006
fuchun198435163.com <tank198435@163.com>
2021-02-19 14:53:03 +0800

    [fuchun] #tech word counter

---
+++ stream-service/src/main/java/com/tank/stream/WordCounter.java
@@ -0,0 +1,41 @@
+package com.tank.stream;
+
+import lombok.NonNull;
+import lombok.SneakyThrows;
+import lombok.val;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.typeinfo.TypeHint;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.tuple.Tuple2;
+
+import java.util.Objects;
+
+/**
+ * @author tank198435163.com
+ */
+public class WordCounter {
+
+  private final String[] words;
+
+  public WordCounter(@NonNull final String... words) {
+    this.words = words;
+  }
+
+  @SneakyThrows({Exception.class})
+  public void count() {
+    Objects.requireNonNull(this.words, "words not allowed empty");
+    val environment = ExecutionEnvironment.getExecutionEnvironment();
+    environment.setParallelism(1);
+
+    environment.fromElements(this.words)
+            .map((MapFunction<String, Tuple2<String, Integer>>) value -> new Tuple2<>(value, 1))
+            .returns(TypeInformation.of(new TypeHint<Tuple2<String, Integer>>() {
+            }))
+            .groupBy(0)
+            .sum(1)
+            .print();
+
+  }
+
+}
