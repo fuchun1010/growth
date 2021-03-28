5bfd4e07c7957e20a5afb470dd87dd9d28b32d53
fuchun198435163.com <tank198435@163.com>
2021-03-28 18:46:27 +0800

    [fuchun] #spike sort quickly

--- gogogo/src/main/java/com/tank/renew/my20210328/QuickSortV1Impl.java
+++ gogogo/src/main/java/com/tank/renew/my20210328/QuickSortV1Impl.java
@@ -22,15 +22,38 @@ public class QuickSortV1Impl<T extends Comparable<T>> implements QuickSortDef<T>
   public T[] sort(@NonNull T[] arr) {
     Preconditions.checkArgument(Objects.nonNull(arr), "array not allowed empty");
     Preconditions.checkArgument(arr.length > 0, "array content not allowed empty");
-    int middleIndex = arr.length / 2;
-    handle(arr, 0, middleIndex);
-    handle(arr, middleIndex + 1, arr.length);
+    this.handle(arr, 0, arr.length);
     return arr;
   }
 
 
   private void handle(@NonNull T[] target, int startIndex, int endIndex) {
+    if (startIndex >= endIndex) {
+      return;
+    }
+    int markIndex = this.pivotIndex(target, startIndex, endIndex);
+    this.handle(target, startIndex, markIndex);
+    this.handle(target, markIndex + 1, endIndex);
+  }
 
+  private int pivotIndex(@NonNull final T[] array, int startIndex, int endIndex) {
+
+    T pivot = array[startIndex];
+    int mark = startIndex;
+
+    for (int index = startIndex + 1; index < endIndex; index++) {
+      T indexValue = array[index];
+      if (indexValue.compareTo(pivot) < 0) {
+        mark++;
+        T tmp = array[index];
+        array[index] = array[mark];
+        array[mark] = tmp;
+      }
+    }
+
+    array[startIndex] = array[mark];
+    array[mark] = pivot;
+    return mark;
   }
 
 
--- gogogo/src/test/java/com/tank/renew/my20210328/QuickSortDefTest.java
+++ gogogo/src/test/java/com/tank/renew/my20210328/QuickSortDefTest.java
@@ -1,5 +1,6 @@
 package com.tank.renew.my20210328;
 
+import lombok.NonNull;
 import org.junit.jupiter.api.*;
 
 /**
@@ -12,16 +13,52 @@ class QuickSortDefTest {
   @Test
   @DisplayName("测试排序")
   void sort() {
+    final Integer[] target = {5, 3, 4, 9, 8, 6, 1};
     final Integer[] result = this.quickSortDef.sort(this.target);
     Assertions.assertEquals(result.length, this.target.length);
     Assertions.assertEquals(result[0].intValue(), 1);
     Assertions.assertEquals(result[result.length - 1].intValue(), 9);
   }
 
+  @Test
+  @DisplayName("找一个短数组排序试试")
+  void sort2() {
+    final Integer[] array = {5, 3, 4, 9, 8};
+    final Integer[] result = this.quickSortDef.sort(array);
+    Assertions.assertEquals(result.length, this.target.length);
+  }
+
+  @Test
+  @DisplayName("挑选基准index")
+  void selectPivotIndex() {
+    int[] array = {5, 3, 4, 9, 8};
+    int markIndex = this.pivotIndex(array, 0, array.length);
+    Assertions.assertEquals(markIndex, 2);
+  }
+
+
   @BeforeEach
   @DisplayName("初始化排序")
   void initialize() {
     this.quickSortDef = new QuickSortV1Impl<>();
+  }
+
+
+  private int pivotIndex(@NonNull final int[] array, int startIndex, int endIndex) {
+
+    int mark = startIndex;
+    int head = array[startIndex];
+
+    for (int index = startIndex + 1; index < endIndex; index++) {
+      int c = array[index];
+      if (c < head) {
+        array[index] = array[index - 1];
+        array[index - 1] = c;
+        mark = index;
+      }
+    }
+
+    return mark;
   }
 
 
